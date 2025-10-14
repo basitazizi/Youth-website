1edd31436381ed24af3b5f557f1bc2d19e9a906d
basitazizi <112999295+basitazizi@users.noreply.github.com>
2025-10-13 23:40:05 -0700

    update and push all

--- firebase-init.js
+++ firebase-init.js
@@ -1,17 +1,7 @@
-// firebase-init.js
+// must run before feed.js and use type="module"
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBd7IJc0K0jqKaNfZBBKF_V5bjY-WCK8oI",
-  authDomain: "youth-website-1d7b0.firebaseapp.com",
-  projectId: "youth-website-1d7b0",
-  storageBucket: "youth-website-1d7b0.appspot.com",
-  messagingSenderId: "109544540041",
-  appId: "1:109544540041:web:52715b2ca691b6de998358",
-  measurementId: "G-WSLJ23FKLT"
-};
-
-const app = initializeApp(firebaseConfig);
-window.firebaseApp = app;                 // <-- expose
-window.firebaseDb  = getFirestore(app);   // <-- expose
+const app = initializeApp({ /* your config */ });
+window.firebaseApp = app;
+window.firebaseDb  = getFirestore(app);
